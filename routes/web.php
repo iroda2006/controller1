27be743ca698c16e5159584fd5664f2d2c3a1b22
irodamirzaxmatova880 <irodamirzaxmatova880@gmail.com>
2025-02-26 12:51:52 +0000

    I added the ProductController.

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+use App\Http\Controllers\ProductController;
+
+Route::resource('products', ProductController::class);
